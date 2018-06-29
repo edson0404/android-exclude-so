a23afef7a5f65e4292429d4812139945dc252894
Edson van Wyk <edson@entersekt.com>
2018-06-29 13:10:16 +0200

    Fixed jween references

---
+++ android-exclude-so-plugin/src/main/groovy/com/android/exclude/so/Rule.groovy
@@ -0,0 +1,19 @@
+package com.android.exclude.so
+
+class Rule {
+    final String name
+    List<String> excludeList = []
+    List<String> includeList = []
+    
+    Rule(String name) {
+        this.name = name
+    }
+    
+    void exclude(String...args) {
+        excludeList += Arrays.asList(args)
+    }
+    
+    void include(String...args) {
+        includeList += Arrays.asList(args)
+    }
+}
